5fde5ca0db8f765a34fc5ab84bdad8d4733d090a
Raphael <rapha.gloria@gmail.com>
2022-05-21 01:51:17 -0300

    basic portfolio

--- js/projects.js
+++ js/projects.js
@@ -4,20 +4,20 @@ const projetos = [
     name: "e-Commerce",
     description: "e-commerce feito em HTML, CSS e Javascript",
     link: "https://rapha021.github.io/marketplace/",
-    img: "../img/ecommerce.png",
+    img: "img/ecommerce.png",
   },
   {
     id: 1,
     name: "Carrinho de compras",
     description: "carrinho de compras feito para praticar o dinamismo com DOM",
     link: "https://rapha021.github.io/carrinho-de-compras/",
-    img: "../img/carrinho.png",
+    img: "img/carrinho.png",
   },
   {
     id: 2,
     name: "Clone página de usuários Netflix - Kenzieflix",
     description: "clone da página de usuários da netflix",
     link: "https://rapha021.github.io/kenzieflix/",
-    img: "../img/kenzieflix.png",
+    img: "img/kenzieflix.png",
   },
 ];
